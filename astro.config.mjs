a4ad702b366369a7f64e0711bc24487377b38da9
adamgroenhout <adamgroenhout@users.noreply.github.com>
2026-03-17 14:44:42 -0400

    chore: update site URL and remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://adamgroenhout.github.io',
-	base: '/new-blog',
+	site: 'https://adamgroenhout.com',
 	integrations: [mdx(), sitemap()],
 });
